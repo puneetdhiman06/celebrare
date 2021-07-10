437b9b915ccab21241f1649e9d2145848ad32068
puneet <puneetdhiman.06@gmail.com>
2021-07-10 22:35:09 +0530

    updated

--- index.js
+++ index.js
@@ -18,7 +18,15 @@ const moveToSlide = (track,currentSlide,targetSlide) =>{
     currentSlide.classList.remove('current-slide');
     targetSlide.classList.add('current-slide');
 }
-
+//
+const flipFunc = (flip,removeFlip)=>{
+    removeFlip.classList.remove('flip');
+    setTimeout(() => {
+        flip.classList.add('flip')
+    }, 300);
+    
+}
+//
 const updateDots = (currentDot,targetDot) => {
 currentDot.classList.remove('current-slide');
 targetDot.classList.add('current-slide');
@@ -42,6 +50,7 @@ else{
     nextButton.classList.remove('is-hidden');
 }
 }
+
 prevButton.addEventListener('click',e=>{
     const currentSlide = track.querySelector('.current-slide');
     const prevSlide = currentSlide.previousElementSibling;
@@ -51,6 +60,9 @@ prevButton.addEventListener('click',e=>{
     moveToSlide(track,currentSlide,prevSlide);
     updateDots(currentDot,prevDot);
     hideShowarrows(slides,prevButton,nextButton,prevIndex);
+    const flipPrev = prevSlide.querySelector('.e-card-cover');
+    const removeFlip = currentSlide.querySelector('.e-card-cover');
+    flipFunc(flipPrev,removeFlip);
 })
 
 nextButton.addEventListener('click',e=>{
@@ -58,11 +70,20 @@ nextButton.addEventListener('click',e=>{
     const nextSlide = currentSlide.nextElementSibling;
     const currentDot = dotsNav.querySelector(".current-slide");
     const nextDot = currentDot.nextElementSibling;
-    const nextIndex = slides.findIndex(slide => slide === nextSlide)
+    const nextIndex = slides.findIndex(slide => slide === nextSlide);
     moveToSlide(track,currentSlide,nextSlide);
     updateDots(currentDot,nextDot);
     hideShowarrows(slides,prevButton,nextButton,nextIndex);
+    const flipNext = nextSlide.querySelector('.e-card-cover');
+    const removeFlip = currentSlide.querySelector('.e-card-cover');
+    flipFunc(flipNext,removeFlip);
 })
+
+
+
+
+
+
 dotsNav.addEventListener('click',e=>{
 const targetDot = e.target.closest('button');
 
@@ -71,8 +92,10 @@ if(!targetDot) return;
 const currentSlide=track.querySelector('.current-slide');
 const currentDot = dotsNav.querySelector('.current-slide');
 const targetIndex = dots.findIndex(dot => dot === targetDot);
-const targetSlide = slides[targetIndex]
+const targetSlide = slides[targetIndex];
+const flip1 = currentSlide.querySelector('.e-card-cover');
 moveToSlide(track,currentSlide,targetSlide);
 updateDots(currentDot,targetDot);
 hideShowarrows(slides,prevButton,nextButton,targetIndex);
+ flip1.classList.add('flip');
 })
