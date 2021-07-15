dcd35539a3137adda1b9d0f12589ca3f4d64e3c9
puneet <puneetdhiman.06@gmail.com>
2021-07-15 11:04:43 +0530

    updated

--- index.js
+++ index.js
@@ -97,13 +97,14 @@ const flip = targetSlide.querySelector('.e-card-cover');
 flipFunc(flip,removeFlip)
 })
 }
+window.dispatchEvent(new Event('resize'));
                                      //scroll button//
 //Get the button
 let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
-
+window.dispatchEvent(new Event('resize'));
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
