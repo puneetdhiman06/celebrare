8977df2caf7a63dd569dbe0d8db630f61c6ea1d3
puneet <puneetdhiman.06@gmail.com>
2022-04-30 14:12:03 +0530

    updated

--- index.js
+++ index.js
@@ -1,104 +1,163 @@
-function carousel(){
-const track = document.querySelector('.carousel-track');
-const slides = Array.from(track.children);
-const nextButton = document.querySelector('.carousel-button-r');
-const prevButton = document.querySelector('.carousel-button-l');
-const dotsNav = document.querySelector('.carousel-nav');
-const dots = Array.from(dotsNav.children);
-
-const slideWidth = slides[0].getBoundingClientRect().width;
-
-const setSlidePosition = (slide,index)=>{
-    slide.style.left = slideWidth * index + 'px';
-}
-
-slides.forEach(setSlidePosition);
-
-const moveToSlide = (track,currentSlide,targetSlide) =>{
-    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';   
-    currentSlide.classList.remove('current-slide');
-    targetSlide.classList.add('current-slide');
-}
-//
-const flipFunc = (flip,removeFlip)=>{
-    removeFlip.classList.remove('flip');
-    setTimeout(() => {
-        flip.classList.add('flip')
-    }, 300);
-}
-//
-const updateDots = (currentDot,targetDot) => {
-currentDot.classList.remove('current-slide');
-targetDot.classList.add('current-slide');
-}
-
-const hideShowarrows = (slides,prevButton,nextButton,targetIndex) => {
+function createCarousel(config){
+    const track = document.querySelector(`#${config.trackId}`);
+    const Slides = Array.from(track.children);
+    const nextButton = document.querySelector(`#${config.buttonRightId}`);
+    const prevButton = document.querySelector(`#${config.buttonLeftId}`);
+    const dotsNav = document.querySelector(`#${config.navId}`);
+    const dots = Array.from(dotsNav.children);
+    const screenWidth = (Slides[0].getBoundingClientRect().width);
+    const setSlidePosition = (slide,index)=>{
+        if (screen && screen.width > 800) {
+            let slideWidth=0;
+                if(config.type==='carousel'){
+                    slideWidth = screenWidth/2.2;
+                                            }
+                else if(config.type==="e-card"){
+                    slideWidth = screenWidth/3.3;
+                                            }
+                
+            slide.style.left = slideWidth * index + 'px';
+        }
+        else if(screen && screen.width < 800) {
+            let slideWidth = screenWidth;
+            slide.style.left = slideWidth * index + 'px';
+        }
+    }
     
-if(targetIndex===0)
-{
-    prevButton.classList.add('is-hidden');
-    nextButton.classList.remove('is-hidden');
+    Slides.forEach(setSlidePosition);
+    
+    const moveToSlide = (track,currentSlide,targetSlide) =>{
+        track.style.transform = 'translateX(-' + targetSlide.style.left + ')';   
+        currentSlide.classList.remove(`${config.type}-current-slide`);
+        targetSlide.classList.add(`${config.type}-current-slide`);
+    }
+    const flipFunc = (flip)=>{
+       if(config.type==='carousel'){
+           setTimeout(() => {
+               flip.classList.add('flip')
+           }, 300);
+       }
+       else return;
+    }
+    const updateDots = (currentDot,targetDot) => {
+    currentDot.classList.remove(`${config.type}-current-slide`);
+    targetDot.classList.add(`${config.type}-current-slide`);
+    }
+    
+    const hideShowarrows = (Slides,prevButton,nextButton,targetIndex) => {
+        
+    if(targetIndex===0)
+    {
+        prevButton.classList.add('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    
+    }
+    
+    else if(targetIndex == Slides.length - 1){
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.add('is-hidden');
+    }
+    else{
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    }
+    }
+    
+    prevButton.addEventListener('click',e=>{
+        const currentSlide = track.querySelector(`.${config.type}-current-slide`);
+        const prevSlide = currentSlide.previousElementSibling;
+        const currentDot = dotsNav.querySelector(`.${config.type}-current-slide`);
+        const prevDot = currentDot.previousElementSibling;
+        const prevIndex = Slides.findIndex(slide => slide === prevSlide);
+        moveToSlide(track,currentSlide,prevSlide);
+        updateDots(currentDot,prevDot);
+        hideShowarrows(Slides,prevButton,nextButton,prevIndex);
 
+        if(config.type==='carousel'){
+            const flipPrev = prevSlide.querySelector('.e-card-cover');
+            flipFunc(flipPrev);
+        }else return;
+
+    })
+    
+    nextButton.addEventListener('click',e=>{
+        const currentSlide = track.querySelector(`.${config.type}-current-slide`);
+        const nextSlide = currentSlide.nextElementSibling;
+        const currentDot = dotsNav.querySelector(`.${config.type}-current-slide`);
+        const nextDot = currentDot.nextElementSibling;
+        const nextIndex = Slides.findIndex(slide => slide === nextSlide);
+        moveToSlide(track,currentSlide,nextSlide);
+        updateDots(currentDot,nextDot);
+        hideShowarrows(Slides,prevButton,nextButton,nextIndex);
+        if(config.type==='carousel'){
+            const flipNext = nextSlide.querySelector('.e-card-cover');
+            flipFunc(flipNext);
+        }else return;
+    })
+    
+    dotsNav.addEventListener('click',e=>{
+    const targetDot = e.target.closest('button');
+    
+    if(!targetDot) return;
+    
+    const currentSlide = track.querySelector(`.${config.type}-current-slide`);
+    const currentDot = dotsNav.querySelector(`.${config.type}-current-slide`);
+    const targetIndex = dots.findIndex(dot => dot === targetDot);
+    const targetSlide = Slides[targetIndex];
+    moveToSlide(track,currentSlide,targetSlide);
+    updateDots(currentDot,targetDot);
+    hideShowarrows(Slides,prevButton,nextButton,targetIndex);
+    if(config.type==='carousel'){
+        const flip = targetSlide.querySelector('.e-card-cover');
+        flipFunc(flip)
+    }
+    })
+    
 }
 
-else if(targetIndex == slides.length - 1){
-    prevButton.classList.remove('is-hidden');
-    nextButton.classList.add('is-hidden');
-}
-else{
-    prevButton.classList.remove('is-hidden');
-    nextButton.classList.remove('is-hidden');
-}
-}
+createCarousel( {trackId:'track-1',
+                buttonRightId:'button-right-1',
+                buttonLeftId:'button-left-1',
+                navId:'nav-1',
+                type:'carousel'});
+createCarousel( {trackId:'track-2',
+                buttonRightId:'button-right-2',
+                buttonLeftId:'button-left-2',
+                navId:'nav-2',
+                type:'carousel'});
+createCarousel( {trackId:'track-3',
+                buttonRightId:'button-right-3',
+                buttonLeftId:'button-left-3',
+                navId:'nav-3',
+                type:'carousel'});
+createCarousel( {trackId:'track-4',
+                buttonRightId:'button-right-4',
+                buttonLeftId:'button-left-4',
+                navId:'nav-4',
+                type:'carousel'});
+                // e-card//
+createCarousel( {trackId:'e-card-track-1',
+                buttonRightId:'e-card-button-right-1',
+                buttonLeftId:'e-card-button-left-1',
+                navId:'e-card-nav-1',
+                type: 'e-card'});
+createCarousel( {trackId:'e-card-track-2',
+                buttonRightId:'e-card-button-right-2',
+                buttonLeftId:'e-card-button-left-2',
+                navId:'e-card-nav-2',
+                type: 'e-card'});
+createCarousel( {trackId:'e-card-track-3',
+                buttonRightId:'e-card-button-right-3',
+                buttonLeftId:'e-card-button-left-3',
+                navId:'e-card-nav-3',
+                type: 'e-card'});
+createCarousel( {trackId:'e-card-track-4',
+                buttonRightId:'e-card-button-right-4',
+                buttonLeftId:'e-card-button-left-4',
+                navId:'e-card-nav-4',
+                type: 'e-card'});
 
-prevButton.addEventListener('click',e=>{
-    const currentSlide = track.querySelector('.current-slide');
-    const prevSlide = currentSlide.previousElementSibling;
-    const currentDot = dotsNav.querySelector(".current-slide");
-    const prevDot = currentDot.previousElementSibling;
-    const prevIndex = slides.findIndex(slide => slide === prevSlide)
-    moveToSlide(track,currentSlide,prevSlide);
-    updateDots(currentDot,prevDot);
-    hideShowarrows(slides,prevButton,nextButton,prevIndex);
-    const flipPrev = prevSlide.querySelector('.e-card-cover');
-    const removeFlip = currentSlide.querySelector('.e-card-cover');
-    flipFunc(flipPrev,removeFlip);
-})
-
-nextButton.addEventListener('click',e=>{
-    const currentSlide = track.querySelector('.current-slide');
-    const nextSlide = currentSlide.nextElementSibling;
-    const currentDot = dotsNav.querySelector(".current-slide");
-    const nextDot = currentDot.nextElementSibling;
-    const nextIndex = slides.findIndex(slide => slide === nextSlide);
-    moveToSlide(track,currentSlide,nextSlide);
-    updateDots(currentDot,nextDot);
-    hideShowarrows(slides,prevButton,nextButton,nextIndex);
-    const flipNext = nextSlide.querySelector('.e-card-cover');
-    const removeFlip = currentSlide.querySelector('.e-card-cover');
-    flipFunc(flipNext,removeFlip);
-})
-
-dotsNav.addEventListener('click',e=>{
-const targetDot = e.target.closest('button');
-
-if(!targetDot) return;
-
-const currentSlide=track.querySelector('.current-slide');
-const currentDot = dotsNav.querySelector('.current-slide');
-const targetIndex = dots.findIndex(dot => dot === targetDot);
-const targetSlide = slides[targetIndex];
-
-moveToSlide(track,currentSlide,targetSlide);
-updateDots(currentDot,targetDot);
-hideShowarrows(slides,prevButton,nextButton,targetIndex);
-const removeFlip = currentSlide.querySelector('.e-card-cover');
-const flip = targetSlide.querySelector('.e-card-cover');
-flipFunc(flip,removeFlip)
-})
-}
-window.dispatchEvent(new Event('resize'));
-                                     //scroll button//
+//scroll button//
 //Get the button
 let mybutton = document.getElementById("myBtn");
 
@@ -118,3 +177,16 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+//more quotes
+const expand=()=>{
+    const inVisible = document.querySelectorAll('.quote-1');
+    const heightIncrease = document.querySelectorAll('.container-5');
+    const selectBtn4 = document.querySelectorAll('.btn-4');
+    for(let j =0;j<heightIncrease.length;j++){
+        heightIncrease[j].style.height="600vh";
+        selectBtn4[j].style.display="none"}
+    for(let i =0;i<inVisible.length-1;i++){inVisible[i].style.display="block";}
+         }
+
